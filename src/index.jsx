aee94091d06e860906c256cf6c299566480af06a
Clement Lakhal <clementlakhal@gmail.com>
2019-06-26 11:35:37 +0200

    Add main stylesheet

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
+import './style/index.scss';
+
 ReactDOM.render(
   <App />,
   document.getElementById('root'),
