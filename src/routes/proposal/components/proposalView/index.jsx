b14e7cf7f3287bfbe576e41c79d3eae4221d73f5
Clement Lakhal <clementlakhal@gmail.com>
2019-06-29 17:45:46 +0200

    Update ProposalView component and style

--- src/routes/proposal/components/proposalView/index.jsx
+++ src/routes/proposal/components/proposalView/index.jsx
@@ -22,6 +22,7 @@ function ProposalView(props) {
     dailyReward,
     challengeProposal,
     goBack,
+    canBeChallenged,
   } = props;
 
   const shortTendermint = `${tendermint.substring(0, 8)}...${tendermint.substring(tendermint.length - 8, tendermint.length)}`;
@@ -102,11 +103,45 @@ function ProposalView(props) {
       </Row>
       <Row className="pb-5">
         <Col>
-          <Button
-            text="Challenge proposal"
-            action={challengeProposal}
-            color="red"
-          />
+          {canBeChallenged ? (
+            <div>
+              <div className="proposal-view__challenge-label">
+                Challenge
+                {' '}
+                <span>
+                  <svg width="12px" height="12px" viewBox="0 0 12 12" version="1.1">
+                    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                      <g id="View-Proposal-Wallet-Connected-Copy" transform="translate(-232.000000, -653.000000)">
+                        <rect fill="#FFFFFF" x="0" y="0" width="1440" height="900" />
+                        <g id="?-copy" transform="translate(232.000000, 653.000000)">
+                          <g id="Group">
+                            <circle id="Oval" fill="#838383" cx="6" cy="6" r="6" />
+                            <path d="M5.255,7.745 C5.255,5.974 7.279,5.589 7.279,4.28 C7.279,3.51 6.685,3.147 5.915,3.147 C5.178,3.147 4.595,3.488 4.364,4.214 L3.495,3.73 C3.88,2.663 4.87,2.157 5.926,2.157 C7.147,2.157 8.302,2.861 8.302,4.236 C8.302,5.93 6.278,6.348 6.278,7.745 L5.255,7.745 Z M5.761,10.121 C5.365,10.121 5.046,9.802 5.046,9.406 C5.046,9.01 5.365,8.691 5.761,8.691 C6.168,8.691 6.476,9.01 6.476,9.406 C6.476,9.802 6.168,10.121 5.761,10.121 Z" id="?" fill="#FFFFFF" fillRule="nonzero" />
+                          </g>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </span>
+              </div>
+              <Button
+                text="Challenge proposal"
+                action={challengeProposal}
+                color="red"
+              />
+            </div>
+          ) : (
+            <div className="proposal-view__already-challenged-label">
+              Someone has already challenged this proposal.
+              <br />
+              View the challenge
+              {' '}
+              <a href="/" className="proposal-view__challenge-link">
+                here
+              </a>
+              .
+            </div>
+          )}
         </Col>
       </Row>
       <Row>
@@ -132,6 +167,7 @@ ProposalView.propTypes = {
   dailyReward: PropTypes.number.isRequired,
   challengeProposal: PropTypes.func.isRequired,
   goBack: PropTypes.func.isRequired,
+  canBeChallenged: PropTypes.bool.isRequired,
 };
 
 export default ProposalView;
--- src/routes/proposal/components/proposalView/index.stories.jsx
+++ src/routes/proposal/components/proposalView/index.stories.jsx
@@ -6,7 +6,20 @@ import ProposalView from '.';
 const deadline = (Date.now() / 1000) + 60 * 60 * 24 * 2;
 
 storiesOf('ProposalView', module)
-  .add('New', () => (
+  .add('Can be challenged', () => (
+    <div className="p-5">
+      <ProposalView
+        tendermint="0x88c0ec80b76c6c0dcaa951ca1089386f7e4a4a7604ca2b5d5ac0d3d5c9787d42"
+        address="0x09c1e4c1adad99436b5c22a395174a1320ee716b"
+        stakeSize="26000"
+        dailyReward="2234"
+        estimatedVotePower="12"
+        deadline={deadline}
+        canBeChallenged
+      />
+    </div>
+  ))
+  .add('Already challenged', () => (
     <div className="p-5">
       <ProposalView
         tendermint="0x88c0ec80b76c6c0dcaa951ca1089386f7e4a4a7604ca2b5d5ac0d3d5c9787d42"
