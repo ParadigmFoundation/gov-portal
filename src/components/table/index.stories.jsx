10571d81f752e25a3aa5612a4e4dfda04497570a
Clement Lakhal <clementlakhal@gmail.com>
2019-07-01 10:01:01 +0200

    Add Table story

--- src/components/table/index.stories.jsx
+++ src/components/table/index.stories.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
+import AcceptedSymbol from '../symbols/acceptedSymbol';
+import RejectedSymbol from '../symbols/rejectedSymbol';
+
 import Table from '.';
 
 storiesOf('Components/Table', module)
-  .add('Standard', () => (
+  .add('Text content', () => (
     <div className="p-5">
       <Table>
         <thead>
@@ -20,6 +23,38 @@ storiesOf('Components/Table', module)
             <td>Foo</td>
             <td>Bar</td>
             <td>Moo</td>
+            <td>Beef</td>
+          </tr>
+          <tr>
+            <td>Foo</td>
+            <td>Bar</td>
+            <td>Moo</td>
+            <td>Beef</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  ))
+  .add('Components content', () => (
+    <div className="p-5">
+      <Table>
+        <thead>
+          <tr>
+            <th>Foo</th>
+            <th>Bar</th>
+            <th>Moo</th>
+            <th>Beef</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <AcceptedSymbol />
+            </td>
+            <td>Bar</td>
+            <td>
+              <RejectedSymbol />
+            </td>
             <td>Beef</td>
           </tr>
           <tr>
