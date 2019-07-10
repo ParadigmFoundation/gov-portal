39a4e28f55d39ce5352bd79a7a2950e89d949572
Clement Lakhal <clementlakhal@gmail.com>
2019-07-11 00:01:44 +0200

    Update ConnectMetaMask component

--- src/components/connectMetaMask/index.jsx
+++ src/components/connectMetaMask/index.jsx
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, {
+  useContext,
+} from 'react';
 import PropTypes from 'prop-types';
+
+import GovContext from '../../store/govContext';
 
 import './index.scss';
 
 function ConnectMetaMask(props) {
   const {
     small,
+    text,
   } = props;
 
+  const gov = useContext(GovContext);
+
+  async function initGov() {
+    if (gov) {
+      try {
+        await gov.init();
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      console.log('Gov is not defined');
+    }
+  }
+
   return (
-    <div className={small ? 'connect-metamask connect-metamask--small' : 'connect-metamask'}>
-      Connect to MetaMask.
+    <div
+      className={small ? 'connect-metamask connect-metamask--small' : 'connect-metamask'}
+      onClick={async () => initGov()}
+      role="button"
+      tabIndex="0"
+      onKeyPress={async () => initGov()}
+    >
+      {text}
     </div>
   );
 }
 
 ConnectMetaMask.propTypes = {
   small: PropTypes.bool,
+  text: PropTypes.string,
 };
 
 ConnectMetaMask.defaultProps = {
   small: false,
+  text: 'Connect to MetaMask.',
 };
 
 export default ConnectMetaMask;
