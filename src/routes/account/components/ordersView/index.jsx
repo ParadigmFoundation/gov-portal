64e1914b7d875e50127d8d64e2e8808301c9cb39
Clement Lakhal <clementlakhal@gmail.com>
2019-07-18 01:37:53 +0200

    Update OrdersView component

--- src/routes/account/components/ordersView/index.jsx
+++ src/routes/account/components/ordersView/index.jsx
@@ -3,11 +3,76 @@ import PropTypes from 'prop-types';
 
 import Table from '../../../../components/table';
 import ConnectMetaMask from '../../../../components/connectMetaMask';
+import EmptyState from '../../../../components/emptyState';
+import ZeroExSymbol from '../../../../components/symbols/zeroExSymbol';
+import DydxSymbol from '../../../../components/symbols/dydxSymbol';
+import DharmaSymbol from '../../../../components/symbols/dharmaSymbol';
+
+import './index.scss';
 
 function OrdersView(props) {
   const {
     metaMaskConnected,
+    orders,
   } = props;
+
+  function returnTypeSymbol(type) {
+    if (type === '0x') {
+      return <ZeroExSymbol />;
+    }
+
+    if (type === 'dY/dX') {
+      return <DydxSymbol />;
+    }
+
+    if (type === 'Dharma') {
+      return <DharmaSymbol />;
+    }
+
+    return 'Unknown type';
+  }
+
+  function displayContent() {
+    if (!metaMaskConnected) {
+      return (
+        <tr>
+          <td colSpan="6">
+            <ConnectMetaMask
+              text="Connect to MetaMask to see your orders."
+              small
+            />
+          </td>
+        </tr>
+      );
+    }
+
+    if (orders.length === 0) {
+      return (
+        <tr>
+          <td colSpan="6">
+            <EmptyState
+              text="No data found"
+              small
+            />
+          </td>
+        </tr>
+      );
+    }
+
+    return orders.map(order => (
+      <tr>
+        <td className="orders-view__id">
+          {`${order.orderId.substring(0, 18)}...`}
+        </td>
+        <td>
+          {returnTypeSymbol(order.type)}
+        </td>
+        <td className="orders-view__expiration">
+          {new Date(order.expiration).toString()}
+        </td>
+      </tr>
+    ));
+  }
 
   return (
     <Table>
@@ -25,16 +90,7 @@ function OrdersView(props) {
         </tr>
       </thead>
       <tbody>
-        {!metaMaskConnected && (
-          <tr>
-            <td colSpan="6">
-              <ConnectMetaMask
-                text="Connect to MetaMask to see your orders."
-                small
-              />
-            </td>
-          </tr>
-        )}
+        {displayContent()}
       </tbody>
     </Table>
   );
@@ -42,10 +98,12 @@ function OrdersView(props) {
 
 OrdersView.propTypes = {
   metaMaskConnected: PropTypes.bool,
+  orders: PropTypes.arrayOf(PropTypes.object),
 };
 
 OrdersView.defaultProps = {
   metaMaskConnected: false,
+  orders: [],
 };
 
 export default OrdersView;
