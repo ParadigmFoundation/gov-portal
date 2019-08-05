dbec3fa2713a51a53dc235cce2b4dffb94b09df7
Clement Lakhal <clementlakhal@gmail.com>
2019-08-05 18:15:17 +0200

    Add Link component

---
+++ src/components/link/index.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  NavLink,
+} from 'react-router-dom';
+import './index.scss';
+
+function Link(props) {
+  const {
+    text,
+    to,
+    color,
+    block,
+    small,
+    disabled,
+  } = props;
+
+  let classname = 'link';
+
+  if (color === 'inverted') {
+    classname += ' link link--inverted';
+  }
+
+  if (color === 'red') {
+    classname += ' link--red';
+  }
+
+  if (color === 'green') {
+    classname += ' link--green';
+  }
+
+  if (color === 'outlined') {
+    classname += ' link--outlined';
+  }
+
+  if (color === 'outlined-green') {
+    classname += ' link--outlined-green';
+  }
+
+  if (small) {
+    classname += ' link--small';
+  }
+
+  if (disabled) {
+    classname += ' link--disabled';
+  }
+
+  if (block) {
+    classname += ' link--block';
+  }
+
+  return (
+    <NavLink
+      to={to}
+      activeClassName={classname}
+      className={classname}
+    >
+      {text}
+    </NavLink>
+  );
+}
+
+Link.propTypes = {
+  text: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  block: PropTypes.bool,
+  small: PropTypes.bool,
+  disabled: PropTypes.bool,
+};
+
+Link.defaultProps = {
+  color: 'default',
+  block: false,
+  small: false,
+  disabled: false,
+};
+
+export default Link;
