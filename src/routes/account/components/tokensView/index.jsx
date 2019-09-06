24258f955cd97170e2d74f135603046f46551025
Clement Lakhal <clementlakhal@gmail.com>
2019-09-06 03:42:01 +0200

    Edit button text

--- src/routes/account/components/tokensView/index.jsx
+++ src/routes/account/components/tokensView/index.jsx
@@ -195,7 +195,7 @@ function TokensView(props) {
                 <Col className="text-right">
                   <Button
                     color="outlined-green"
-                    text="Bond"
+                    text={bondedTokens === '0' ? 'Bond' : 'Edit'}
                     disabled={!metaMaskConnected}
                     action={() => setIsBondModalOpen(true)}
                     small
