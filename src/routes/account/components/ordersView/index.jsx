554707b941ac594a8b843b5ad2d574041748290a
Clement Lakhal <clementlakhal@gmail.com>
2019-09-24 16:40:49 +0200

    Remove useless function and imports

--- src/routes/account/components/ordersView/index.jsx
+++ src/routes/account/components/ordersView/index.jsx
@@ -4,9 +4,6 @@ import PropTypes from 'prop-types';
 import Table from '../../../../components/table';
 import ConnectMetaMask from '../../../../components/connectMetaMask';
 import EmptyState from '../../../../components/emptyState';
-import ZeroExSymbol from '../../../../components/symbols/zeroExSymbol';
-import DydxSymbol from '../../../../components/symbols/dydxSymbol';
-import DharmaSymbol from '../../../../components/symbols/dharmaSymbol';
 
 import './index.scss';
 
@@ -15,22 +12,6 @@ function OrdersView(props) {
     metaMaskConnected,
     orders,
   } = props;
-
-  function returnTypeSymbol(type) {
-    if (type === '0x') {
-      return <ZeroExSymbol />;
-    }
-
-    if (type === 'dY/dX') {
-      return <DydxSymbol />;
-    }
-
-    if (type === 'Dharma') {
-      return <DharmaSymbol />;
-    }
-
-    return 'Unknown type';
-  }
 
   function displayContent() {
     if (!metaMaskConnected) {
