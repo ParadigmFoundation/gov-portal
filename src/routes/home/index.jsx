2cdd4e2e517d242785e47bea0fd12dcefa70d73a
Clement Lakhal <clementlakhal@gmail.com>
2019-10-02 02:15:41 +0200

    Fix order display (from most recent to oldest)

--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -19,13 +19,27 @@ function Home() {
     pastChallenges,
   } = useContext(GovContext);
 
+  function compareProposals(a, b) {
+    const unixA = a.acceptUnix;
+    const unixB = b.acceptUnix;
+
+    return unixB - unixA;
+  }
+
+  function compareChallenges(a, b) {
+    const unixA = a.id;
+    const unixB = b.id;
+
+    return unixB - unixA;
+  }
+
   return (
     <div>
       <HomeView
         metaMaskConnected={isReady}
-        proposals={proposals}
+        proposals={proposals.sort(compareProposals)}
         validators={validators}
-        activeChallenges={activeChallenges}
+        activeChallenges={activeChallenges.sort(compareChallenges)}
         pastChallenges={pastChallenges}
       />
     </div>
